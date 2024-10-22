42b2dcd736433ff05a4a14b0c79813e799bb5094
ANJUCAMO-77 <anjucamo77@gmail.com>
2024-10-21 19:05:33 -0500

    feat: se implementa listar proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Administracion from "./pages/Administracion";
 import AppLayout from "./layout/AppLayout";
 import AuthLayout from "./layout/AuthLayout";
 import ResetPassword from "./pages/auth/ResetPassword";
+import AdministracionProyecto from "./components/AdministracionProyecto";
 import ProyectPage from "./pages/proyects/ProyectPage";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
             <Route path="/recuperacion" element={<RecuperacionPage />} />
             <Route path="/recuperacion/:token" element={<ResetPassword />} />
             <Route path="/proyecto" element={<ProyectPage />} />
+            <Route path="/proyectos" element={<AdministracionProyecto />} />
           </Route>
 
           <Route element={<AppLayout />}>
